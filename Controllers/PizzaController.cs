3c4a42472caea9a898697f581345601b6ed11acc
Tshikwetamakole <105504810+Tshikwetamakole@users.noreply.github.com>
2025-02-28 09:13:39 +0200

    Add project files.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaAPI.Models;
+using PizzaAPI.Services;
+
+namespace PizzaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        private readonly PizzaService _pizzaService;
+
+        public PizzaController(PizzaService pizzaService)
+        {
+            _pizzaService = pizzaService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => _pizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> GetById(int id)
+        {
+            var pizza = _pizzaService.GetById(id);
+            return pizza != null ? Ok(pizza) : NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Pizza pizza)
+        {
+            _pizzaService.Add(pizza);
+            return CreatedAtAction(nameof(GetById), new
+            {
+                id = pizza.Id
+            }, pizza);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            var existingPizza = _pizzaService.GetById(id);
+            if(existingPizza == null)
+                return NotFound();
+
+            _pizzaService.Update(id, pizza);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = _pizzaService.GetById(id);
+            if(pizza == null)
+                return NotFound();
+
+            _pizzaService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Service/PizzaService.cs
@@ -0,0 +1,39 @@
+﻿using PizzaAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaAPI.Services
+{
+    public class PizzaService
+    {
+        private static List<Pizza> pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Margherita", Description = "Classic cheese and tomato", Price = 8.99M, IsVegetarian = true },
+            new Pizza { Id = 2, Name = "Pepperoni", Description = "Pepperoni and cheese", Price = 10.99M, IsVegetarian = false }
+        };
+
+        public List<Pizza> GetAll() => pizzas;
+
+        public Pizza? GetById(int id) => pizzas.FirstOrDefault(p => p.Id == id);
+
+        public void Add(Pizza pizza)
+        {
+            pizza.Id = pizzas.Count + 1;
+            pizzas.Add(pizza);
+        }
+
+        public void Update(int id, Pizza pizza)
+        {
+            var existingPizza = pizzas.FirstOrDefault(p => p.Id == id);
+            if(existingPizza != null)
+            {
+                existingPizza.Name = pizza.Name;
+                existingPizza.Description = pizza.Description;
+                existingPizza.Price = pizza.Price;
+                existingPizza.IsVegetarian = pizza.IsVegetarian;
+            }
+        }
+
+        public void Delete(int id) => pizzas.RemoveAll(p => p.Id == id);
+    }
+}
